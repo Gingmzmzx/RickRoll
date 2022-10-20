9f845168cbf64a91081e08c3d522e20bf28dbd95
xzystudio <49107602+Gingmzmzx@users.noreply.github.com>
2022-10-20 22:31:47 +0800

    Add files via upload

---
+++ rickroll.py
@@ -0,0 +1,24 @@
+class GiveClass:
+    def give(self):
+        return self
+    
+    def you(self):
+        return Never()
+
+class Never:
+    Gonna = None
+    def __init__(self):
+        self.Gonna = GiveClass()
+    
+    def up(self):
+        print("sussy baka")
+
+class RickRoll(Never):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def Never(self):
+        return Never()
+
+# How to use?
+RickRoll().Never().Gonna.give().you().up()
